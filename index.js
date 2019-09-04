c3a800b1444d8d340dd1a6fc059db4706c9099cf
Jacob W <417799218@qq.com>
2019-09-04 03:48:14 -0600

    add logic for search bar appearing

---
+++ index.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var searchInput =document.getElementById("searchInput");
+    searchInput.addEventListener("keyup",showKeyword,false)
+    searchInput.addEventListener("blur",hideKeyword,false)
+    searchInput.addEventListener("focus",showKeyword,false)
+
+    function showKeyword() {
+        if(searchInput.value !==""){
+            document.getElementById("search-suggest").style.display="block";
+
+        }
+// logic: if value not equal empty we do next
+    }
+    function hideKeyword() {
+        document.getElementById("search-suggest").style.display="none";
+    //lose focus makes element undisplayed
+
+    }
+
+}
